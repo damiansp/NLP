920334cf57dbb23456b46f3ac0ebe0dceb4f71ce
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-06-20 18:29:18 -0400

    Added the Viterbi algorithm for decoding, that is, given a sequence of observations, return the sequence of states with the highest probability

--- models/hidden_markov_chain.py
+++ models/hidden_markov_chain.py
@@ -69,6 +69,86 @@ def hmm_forward(Q, A, B, Obs, pi):
 
     return lattice
 
+
+
+def hmm_viterbi(Q, A, B, Obs, pi):
+    '''                                                                             
+    Using the Viterbi algorithm to compute the sequence of observations (Obs) using
+    a Hidden Markov Model (HMM) with a given state transition probability matrix (A)
+    and emission probability matrix (B).          
+                                                                                    
+    @param Q (list):                                                                
+      the set of N states [q1, q2, ..., qN]                                         
+      *** NOTE: values should correspond to row indices in A ***                    
+    @param A (matrix):                                                              
+      the transition probability matrix:                                            
+      each a[i, j] represents the prob. of moving from state i to state j           
+      s.t. sum[j](a[i, j]) = 1 for all i                                            
+    @param B (matrix):                                                              
+      sequence of observation likelihoods ("emission probabilities"),               
+      b[i](o[t]); the probabilities of observations o[t] being generated from       
+      state i                                                                       
+    @param Obs (list):                                                              
+      sequence of T observations                                                    
+      ** NOTE: values should correspond to row indices in B ***                     
+    @param pi (list):                                                               
+      initial probability distribution (maps to Q)                                  
+                                                                                    
+    @return backtrace (list):
+      the sequence of states with the highest probability
+    '''
+    # make sure A and B are numpy arrays
+    A = np.array(A)
+    B = np.array(B)
+    
+    N = len(Q)
+    T = len(Obs)
+    lattice = np.zeros(shape = (N, T))
+    backtrace = np.zeros(shape = (N, T))
+    
+    # Initialize first column of lattice (time = 0; start)
+    for q in range(N):
+        lattice[q, 0] = pi[q] * B[Obs[0], q]
+        backtrace[q, 0] = pi[q]
+        
+    # Fill in the rest of the lattice
+    for t in range(1, T):
+        # q: state at time t - 1
+        for q in range(N):
+            node_val = 0
+            bt_val = 0
+            # q2: state at time t
+            for q2 in range(N):
+                # alpha = the value at the nodes in the previous column
+                alpha = lattice[q, t - 1]
+                # a = the probability of being at current state given a
+                # previous state
+                a = A[q2, q]
+                # b = the probability of a given observation given the current
+                # state
+                b = B[Obs[t], q2]
+                # The probability of being at a current state given a prior
+                # state is thus alpha * a * b; sum over all possible prior
+                # states
+                test_bt = alpha * a
+                test_val = test_bt * b
+
+                if test_bt > bt_val:
+                    bt_val = test_bt
+                    
+                if test_val > node_val:
+                    node_val = test_val
+
+                    
+            lattice[q, t] = node_val
+            backtrace[q, t] = bt_val
+                    
+    return backtrace
+                
+                
+
+
+
 # Example: Suppose we have an unknown sequence of consecutive days  (e.g.,
 # { hot, warm, cold, ... } but have receipts for Max's daily ice cream
 # purchases over the same time period.
@@ -100,6 +180,9 @@ H2 = hmm_forward(Q, A, B, Obs2, pi)
 print 'Hypothesis 1 Probability Matrix:\n', H1
 print 'Hypothesis 2 Probability Matrix:\n', H2
 
-# TO DO:
-# Hand check a small set to make sure calculations came out correctly
-# See if vectorization can be done to improve efficiency
+
+bt1 = hmm_viterbi(Q, A, B, Obs1, pi)
+bt2 = hmm_viterbi(Q, A, B, Obs2, pi)
+
+print 'bt1:\n', bt1
+print 'bt2:\n', bt2
