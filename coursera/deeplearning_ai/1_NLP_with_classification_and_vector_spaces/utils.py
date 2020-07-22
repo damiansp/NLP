abaf755f40616220db296702c09f314de697fc74
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-07-21 18:04:08 -0700

    update utils and +rot mats

--- coursera/deeplearning_ai/1_NLP_with_classification_and_vector_spaces/utils.py
+++ coursera/deeplearning_ai/1_NLP_with_classification_and_vector_spaces/utils.py
@@ -2,9 +2,11 @@ import json
 import re
 import string
 
+import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import TweetTokenizer
+import numpy as np
 
 
 def process_tweet(tweet):
@@ -75,3 +77,71 @@ def get_all_tweets(directory, pos_neg):
             tweet = json.loads(line)
             all_tweets.append(tweet['text'])
     return all_tweets
+
+
+def get_dict(file_name):
+    '''
+    This function returns the english to french dictionary given a file where 
+    the each column corresponds to a word.
+    Check out the files this function takes in your workspace.
+    '''
+    my_file = pd.read_csv(file_name, delimiter=' ')
+    etof = {}  # the english to french dictionary to be returned
+    for i in range(len(my_file)):
+        en = my_file.loc[i][0]
+        fr = my_file.loc[i][1]
+        etof[en] = fr
+    return etof
+
+
+def cosine_similarity(A, B):
+    '''
+    Input:
+      A: a numpy array which corresponds to a word vector
+      B: A numpy array which corresponds to a word vector
+    Output:
+      cos: numerical number representing the cosine similarity between A and B.
+    '''
+    # you have to set this variable to the true label.
+    cos = -10
+    dot = np.dot(A, B)
+    norma = np.linalg.norm(A)
+    normb = np.linalg.norm(B)
+    cos = dot / (norma * normb)
+    return cos
+
+
+# Procedure to plot and arrows that represents vectors with pyplot
+def plot_vectors(
+        vectors, colors=['k', 'b', 'r', 'm', 'c'], axes=None, fname='image.svg',
+        ax=None):
+    scale = 1
+    scale_units = 'x'
+    x_dir = []
+    y_dir = []
+    for i, vec in enumerate(vectors):
+        x_dir.append(vec[0][0])
+        y_dir.append(vec[0][1])
+    if ax == None:
+        fig, ax2 = plt.subplots()
+    else:
+        ax2 = ax
+    if axes == None:
+        x_axis = 2 + np.max(np.abs(x_dir))
+        y_axis = 2 + np.max(np.abs(y_dir))
+    else:
+        x_axis = axes[0]
+        y_axis = axes[1]
+    ax2.axis([-x_axis, x_axis, -y_axis, y_axis])
+    for i, vec in enumerate(vectors):
+        ax2.arrow(0,
+                  0,
+                  vec[0][0],
+                  vec[0][1],
+                  head_width=0.05 * x_axis,
+                  head_length=0.05 * y_axis,
+                  fc=colors[i],
+                  ec=colors[i])
+    if ax == None:
+        plt.show()
+        fig.savefig(fname)
