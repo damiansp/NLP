0f024bf7922adf81ffba4bcd7f926b61c7b5f51f
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-11-29 07:59:03 -0800

    init tour

---
+++ transformers/01_hello/tour.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+
+text = """Dear Amazon, last week I ordered an Optimus Prime action figure \
+from your online store in Germany. Unfortunately, when I opened the package, \
+I discovered to my horror that I had been sent an action figure of Megatron \
+instead! As a lifelong enemy of the Decepticons, I hope you can understand my \
+dilemma. To resolve the issue, I demand an exchange of Megatron for the \
+Optimus Prime figure I ordered. Enclosed are copies of my records concerning \
+this purchase. I expect to hear from you soon. Sincerely, Bumblebee."""
+
+
+classifier = pipeline('sentiment-analysis')
