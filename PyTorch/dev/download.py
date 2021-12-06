8c7d33581f4ef353af3a4898b1536aeaf3c9fa16
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-12-06 11:51:07 -0800

    reorg

--- PyTorch/dev/download.py
+++ PyTorch/dev/download.py
@@ -8,12 +8,12 @@ def progress_bar(some_iter):
     try:
         from tqdm import tqdm
         return tqdm(some_iter)
-    except: #ModuleNotFoundError:
+    except ModuleNotFoundError:
         return some_iter
 
-    
+
 def download_file_from_google_drive(id, destination):
-    print("Trying to fetch {}".format(destination))
+    print(f'Trying to fetch {destination}')
 
     def get_confirm_token(response):
         for key, value in response.cookies.items():
@@ -23,25 +23,25 @@ def download_file_from_google_drive(id, destination):
 
     def save_response_content(response, destination):
         CHUNK_SIZE = 32768
-        with open(destination, "wb") as f:
+        with open(destination, 'wb') as f:
             for chunk in progress_bar(response.iter_content(CHUNK_SIZE)):
                 if chunk: # filter out keep-alive new chunks
                     f.write(chunk)
 
     URL = "https://docs.google.com/uc?export=download"
     session = requests.Session()
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params = {'id': id}, stream=True)
     token = get_confirm_token(response)
     if token:
-        params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
     save_response_content(response, destination)
 
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) is not 3:
-        print("Usage: python download.py drive_file_id destination_file_path")
+        print('Usage: python download.py drive_file_id destination_file_path')
     else:
         # TAKE ID FROM SHAREABLE LINK
         file_id = sys.argv[1]
